15dc5d354dccc6eb75d0e58dd1659637104b70cd
josipglavas <glavas.josip@icloud.com>
2025-03-24 18:33:25 +0100

    napisan zadatak koji ispisuje povrsinu pravokutnika

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        double povrsina = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Unesi duljinu pravokutnika:");
+        Double duljina = Double.parseDouble(input.nextLine());
+
+        System.out.println("Unesi širinu pravokutnika:");
+        Double sirina = Double.parseDouble(input.nextLine());
+
+        input.close();
+
+        povrsina = duljina * sirina;
+
+        System.out.println("površina pravokutnika je " + povrsina);
+    }
+}
